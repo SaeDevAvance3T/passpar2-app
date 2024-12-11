1ba46501705cb6057609bf6905ec589092950f30
THOMAS IZARD <thomas.izard@iut.local>
2024-12-11 14:55:11 +0100

    tentative debug api sur pc iut

--- app/src/main/java/com/example/passpar2/MainActivity.java
+++ app/src/main/java/com/example/passpar2/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.passpar2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,8 +17,11 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -29,9 +36,9 @@ public class MainActivity extends AppCompatActivity {
     /**
      * URL de l'API à interroger
      */
-    private static final String URL_FILM = "http://www.omdbapi.com/?t=%s&apikey=XXXXXXXX";
+    private static final String URL_FILM = "http://10.0.2.2:8080/api/customers";
 
-    /** Zone de saisie du titre du film recherché */
+    /** Zone de saisie du titre recherché */
     private EditText zoneTitre;
 
     /** Zone pour afficher le résultat de la recherche */
@@ -42,12 +49,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.nouveau_compte_partie1);
+        setContentView(R.layout.nouveau_compte_partie1); // Assure-toi que ce layout existe
 
         zoneTitre = findViewById(R.id.saisieNom);
         zoneResultat = findViewById(R.id.titre);
-
         boutonConnecter = findViewById(R.id.nouveau_compte_connecter);
+
+        boutonConnecter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicRechercherEnChaine(v);
+            }
+        });
     }
 
     /**
@@ -61,50 +74,76 @@ public class MainActivity extends AppCompatActivity {
         if (fileRequete == null) {
             fileRequete = Volley.newRequestQueue(this);
         }
-        // sinon
         return fileRequete;
     }
 
     /**
-     * Gestion du clic sur le bouton rechercher
-     * Une requête est envoyée au Web service pour rechercher le film saisi par l'utilisateur.
-     * Le résultat de la requête est affiché en sous la forme d'une chaîne de caractères.
-     * A défaut, c'est un message d'erreur qui est affiché
-     * @param bouton bouton à l'origine du clic
+     * Vérifie si l'appareil a une connexion Internet disponible
+     * @return true si la connexion est disponible, false sinon
      */
-    public void clicRechercherEnChaine(View bouton) {
-        try {
-            // le titre saisi par l'utilisateur est récupéré et encodé en UTF-8
-            String titre = URLEncoder.encode(zoneTitre.getText().toString(), "UTF-8");
-            // le titre du film est insésré dans l'URL de recherche du film
-            String url = String.format(URL_FILM, titre);
-            /*
-             * on crée une requête GET, paramètrée par l'url préparée ci-dessus,
-             * Le résultat de cette requête sera une chaîne de caractères, donc la requête
-             * est de type StringRequest
-             */
-            StringRequest requeteVolley = new StringRequest(Request.Method.GET, url,
-                    // écouteur de la réponse renvoyée par la requête
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String reponse) {
-                            zoneResultat.setText("Début de la réponse obtenue"
-                                    + reponse.substring(0, Math.min(400, reponse.length())));
-                        }
-                    },
-                    // écouteur du retour de la requête si aucun résultat n'est renvoyé
-                    new Response.ErrorListener() {
-                        @Override
-                        public void onErrorResponse(VolleyError erreur) {
-                            zoneResultat.setText("R.string.message_erreur");
-                        }
-                    });
-            // la requête est placée dans la file d'attente des requêtes
-            getFileRequete().add(requeteVolley);
-        } catch(UnsupportedEncodingException erreur) {
-            // problème lors de l'encodage de la chaîne titre
-            Toast.makeText(this, "erreur", Toast.LENGTH_LONG).show();
+    public boolean estConnecteInternet() {
+        ConnectivityManager gestionnaireConnexion = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo infoReseau = gestionnaireConnexion.getActiveNetworkInfo();
+
+        if (infoReseau != null && infoReseau.isConnected()) {
+            return true;
+        } else {
+            Toast.makeText(this, "Pas de connexion Internet", Toast.LENGTH_SHORT).show();
+            return false;
         }
     }
 
+    /**
+     * Gestion du clic sur le bouton rechercher
+     * Une requête est envoyée au Web service pour rechercher ce qui est saisi par l'utilisateur.
+     * @param bouton bouton à l'origine du clic
+     */
+    public void clicRechercherEnChaine(View bouton) {
+        // Vérifier la connexion Internet avant d'envoyer la requête
+        if (!estConnecteInternet()) {
+            return;
+        }
+
+        String urlPrete = URL_FILM;
+
+        JsonObjectRequest requeteVolley = new JsonObjectRequest(
+                Request.Method.GET, urlPrete, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject reponse) {
+
+                        setZoneResultatAvecObjetJson(reponse);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError erreur) {
+                        erreur.printStackTrace();
+                        zoneResultat.setText(erreur.toString());
+                        Toast.makeText(MainActivity.this, erreur.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // Ajout de la requête à la file d'attente de Volley
+        Volley.newRequestQueue(this).add(requeteVolley);
+    }
+
+    /**
+     * Affiche dans la zone de résultat, les informations essentielles associées à la réponse JSON
+     * @param reponse Objet JSON contenant la réponse de l'API
+     */
+    public void setZoneResultatAvecObjetJson(JSONObject reponse) {
+        try {
+            StringBuilder resultatFormate = new StringBuilder();
+            if (reponse.has("nom")) {
+                resultatFormate.append("Nom : ").append(reponse.getString("nom"));
+            } else {
+                resultatFormate.append("Nom non disponible");
+            }
+            zoneResultat.setText(resultatFormate.toString());
+        } catch (JSONException erreur) {
+            Log.e("JSONError", "Erreur lors de l'analyse du JSON", erreur);
+            Toast.makeText(this, "Erreur lors de l'analyse du JSON", Toast.LENGTH_LONG).show();
+        }
+    }
 }
