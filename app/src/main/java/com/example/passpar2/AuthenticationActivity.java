b73095e862964e5d414f4c010983ac6b72a2dadc
THOMAS IZARD <thomas.izard@iut.local>
2025-01-28 15:48:22 +0100

    Connexion API

--- app/src/main/java/com/example/passpar2/AuthenticationActivity.java
+++ app/src/main/java/com/example/passpar2/AuthenticationActivity.java
@@ -15,37 +15,28 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 public class AuthenticationActivity extends AppCompatActivity {
 
-    /** Contient le champ email du formulaire */
     private EditText emailArea;
-
-    /** Contient le champ mot de passe du formulaire */
     private EditText passwordArea;
-
-    /** contient la reponse de l'API */
     private TextView responseArea;
-
-    /** Contient la chaine de caractères du champ email */
     private String emailText;
-
-    /** Contient la chaine de caractères du champ mot de passe */
     private String passwordText;
-
-    /** Contient l'url de l'appel à l'API */
     private String url;
-
-    /** Contient les paramètres du corps de la requête */
     private String[] bodyRequest;
 
     @Override
@@ -60,7 +51,7 @@ public class AuthenticationActivity extends AppCompatActivity {
     }
 
     public void createAccountClick(View view) {
-        //Intent intention = new Intent(AuthenticationActivity.class, )
+        // L'intention pour créer un compte n'est pas encore définie
     }
 
     public void authenticationClick(View view) {
@@ -69,9 +60,7 @@ public class AuthenticationActivity extends AppCompatActivity {
         if (emailText.isEmpty() || passwordText.isEmpty()) {
             responseArea.setText(R.string.missing_data);
         } else {
-            Toast.makeText(this, emailText + passwordText, Toast.LENGTH_LONG)
-                    .show();
-            url = "https://postman-echo.com/post";
+            url = "https://2bet.fr/api/login";
             bodyRequest[0] = emailText;
             bodyRequest[1] = passwordText;
             sendData(url, bodyRequest);
@@ -79,43 +68,70 @@ public class AuthenticationActivity extends AppCompatActivity {
     }
 
     public void sendData(String url, String[] body) {
-        boolean toutOk;
-        toutOk = true;
+
+
+
+        boolean toutOk = true;
         JSONObject objectSent = new JSONObject();
         try {
-            objectSent.put("EMAIL", body[0]);
-            objectSent.put("PASSWORD", body[1]);
-        } catch(JSONException e) {
+            objectSent.put("email", body[0]);
+            objectSent.put("password", body[1]);
+        } catch (JSONException e) {
             toutOk = false;
         }
 
         if (toutOk) {
             JsonObjectRequest volleyRequest = new JsonObjectRequest(Request.Method.POST, url, objectSent,
                     new Response.Listener<JSONObject>() {
-                        public void onResponse(JSONObject reponse) {
-                            responseArea.setText(R.string.api_check);
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                JSONObject responseData = response.getJSONObject("response");
+
+                                // Extraire les données de l'objet "response"
+                                int id = responseData.getInt("id");
+                                String firstName = responseData.getString("firstName");
+                                String lastName = responseData.getString("lastName");
+                                String email = responseData.getString("email");
+
+                                // Construire le message à afficher dans l'UI
+                                String message = "ID: " + id + "\n" +
+                                        "First Name: " + firstName + "\n" +
+                                        "Last Name: " + lastName + "\n" +
+                                        "Email: " + email;
+
+                                // Afficher les données dans la TextView
+                                responseArea.setText(message);
+                            } catch (JSONException e) {
+                                responseArea.setText("Erreur lors de la lecture de la réponse.");
+                            }
                         }
                     },
                     new Response.ErrorListener() {
+                        @Override
                         public void onErrorResponse(VolleyError error) {
-                            responseArea.setText("erreur");
+                            responseArea.setText("Erreur API : " + error.getMessage());
                         }
-                    })
+                    }) {
 
-            {
                 @Override
                 public Map<String, String> getHeaders() throws AuthFailureError {
                     HashMap<String, String> headers = new HashMap<>();
-
-                    //headers.put();
+                    headers.put("Content-Type", "application/json");  // Ajout du Content-Type
                     return headers;
                 }
             };
+
+            // Initialiser la RequestQueue et ajouter la requête
+            RequestQueue queue = Volley.newRequestQueue(this);
+            queue.add(volleyRequest);
+        } else {
+            responseArea.setText("Erreur lors de la création de l'objet JSON.");
         }
     }
 
     public void forgottenPasswordClick(View view) {
-        Toast.makeText(this, "un mail vous a été envoyé à l'adresse mail renseignée", Toast.LENGTH_LONG)
+        Toast.makeText(this, "Un mail vous a été envoyé à l'adresse mail renseignée", Toast.LENGTH_LONG)
                 .show();
     }
 }
