0bfdd788d5d79d13e4d44373040b40e9acfcf3a6
TONY LAPEYRE <tony.lapeyre@iut.local>
2025-02-13 09:18:36 +0100

    page finale nouvel itinéraire

--- app/src/main/java/com/example/passpar2/CheckboxAdapter.java
+++ app/src/main/java/com/example/passpar2/CheckboxAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
+import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.HashSet;
--- app/src/main/java/com/example/passpar2/NewRouteActivity.java
+++ app/src/main/java/com/example/passpar2/NewRouteActivity.java
@@ -1,20 +1,27 @@
 package com.example.passpar2;
 
+import static android.widget.Toast.LENGTH_LONG;
+
 import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -23,12 +30,15 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.StringJoiner;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
@@ -40,6 +50,8 @@ public class NewRouteActivity extends AppCompatActivity implements CheckboxSelec
     /** Contient l'URL appelant l'API  */
     private final String URL_ENTERPRISES = "https://2bet.fr/api/customers?user=0";
 
+    private final String URL_ITINERARY = "https://2bet.fr/api/itineraries";
+
     /** Clé pour le nombre transmis par l'activité fille */
     public final static String CLE_NOMBRE = "NOMBRE";
 
@@ -49,6 +61,10 @@ public class NewRouteActivity extends AppCompatActivity implements CheckboxSelec
      * File d'attente pour les requêtes Web (en lien avec l'utilisation de Volley)
      */
     private RequestQueue fileRequete;
+
+    public EditText itineraryName;
+
+    public TextView itineraryLabel;
 
     /**  Contient les entreprise sélectionnées par l'utilisateur avec les checkboxs */
     private ArrayList<Integer> selectedEnterprises;
@@ -61,6 +77,7 @@ public class NewRouteActivity extends AppCompatActivity implements CheckboxSelec
     public TextView displayedEnterprises;
 
     private CheckboxAdapter adapter;
+    public TextView textChoice;
 
 
 
@@ -69,11 +86,19 @@ public class NewRouteActivity extends AppCompatActivity implements CheckboxSelec
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_route_layout);
 
+        itineraryName = findViewById(R.id.itinerary_name);
+
+        itineraryLabel = findViewById(R.id.itinerary_label);
+
         enterpriseList = findViewById(R.id.enterpriselist);
 
         displayedEnterprises = findViewById(R.id.displayed_enterprises);
 
+        textChoice = findViewById(R.id.text_choice);
+
         enterpriseValues = new HashMap<>();
+
+        selectedEnterprises = new ArrayList<>();
 
         // Désactiver la vérification du certificat SSL pour les tests
         TrustManager[] trustAllCertificates = new TrustManager[]{
@@ -98,19 +123,7 @@ public class NewRouteActivity extends AppCompatActivity implements CheckboxSelec
             e.printStackTrace();
         }
 
-        //enterpriseValues.put(1, "Sopra");
-        //enterpriseValues.put(2, "CA");
-        //enterpriseValues.put(3, "Ledger");
-        //enterpriseValues.put(4, "SCH");
-        //enterpriseValues.put(5, "Freeze Corleone");
-        //enterpriseValues.put(6, "Thomas");
-        //enterpriseValues.put(7, "Jérome");
-        //enterpriseValues.put(8, "Jérome");
-
         getEnterpriseList();
-
-        adapter = new CheckboxAdapter(this, enterpriseValues, this);
-        enterpriseList.setAdapter(adapter);
 
         /*
          * Lorsque l'utilisateur cliquera sur la touche back du téléphone pour revenir
@@ -160,6 +173,9 @@ public class NewRouteActivity extends AppCompatActivity implements CheckboxSelec
                     public void onResponse(JSONObject response) {
                         // Effacer la liste des clients existante
                         enterpriseValues.clear();
+                        displayedEnterprises.setTextColor(Color.parseColor("#FFFFFF"));
+                        displayedEnterprises.setText("");
+                        Log.d("MainActivity", "réponse récupérée: " + response);
                         try {
                             // Accéder à la clé 'response' qui contient le tableau des clients
                             JSONArray enterpriseArray = response.getJSONArray("response");
@@ -170,12 +186,17 @@ public class NewRouteActivity extends AppCompatActivity implements CheckboxSelec
 
                                 // Récupérer le nom et la description du client
                                 String id = clientJson.optString("id", "Id non disponible");
+                                Log.d("MainActivity", "id récupéré: " + id);
                                 String name = clientJson.optString("name", "Nom de l'entreprise non disponible");
+                                Log.d("MainActivity", "nom récupéré: " + name);
 
                                 // Ajouter les clients dans la liste
                                 enterpriseValues.put(Integer.parseInt(id), name);
                             }
-
+                            adapter = new CheckboxAdapter(NewRouteActivity.this, enterpriseValues, NewRouteActivity.this);
+                            enterpriseList.setAdapter(adapter);
+                            adapter.notifyDataSetChanged();
+                            Log.d("MainActivity", "hashmap: " + enterpriseValues);
                         } catch (Exception e) {
                             e.printStackTrace();
                             Toast.makeText(NewRouteActivity.this, "Erreur lors de la récupération des entreprises", Toast.LENGTH_SHORT).show();
@@ -195,6 +216,8 @@ public class NewRouteActivity extends AppCompatActivity implements CheckboxSelec
                             Log.e("VolleyError", "Erreur réseau inconnue");
                         }
                         Toast.makeText(NewRouteActivity.this, "Erreur de connexion", Toast.LENGTH_SHORT).show();
+                        displayedEnterprises.setText("Aucun client disponible");
+                        displayedEnterprises.setTextColor(Color.parseColor("#FF0000"));
                     }
                 });
         // la requête est placée dans la file d'attente des requêtes
@@ -208,8 +231,91 @@ public class NewRouteActivity extends AppCompatActivity implements CheckboxSelec
     public void checkClick(View view) {
 
         if (selectedEnterprises.size() != 0) {
-            //Intent intention = new Intent(NewRouteActivity.class, )
-            //intention.putExtra(EXTRA_ENTERPRISE, selectedEnterprises);
+            if (!itineraryName.getText().toString().isEmpty()) {
+
+                boolean toutOk;
+                /*
+                 * préparation du nouveau client, à ajouter, en tant qu'objet Json
+                 * Les informations le concernant sont renseignées avec des valeurs par défaut,
+                 * sauf le nom du magasin qui est celui renseigné par l'utilisateur
+                 */
+                toutOk = true;
+                JSONObject sentObject = new JSONObject();
+                try {
+                    SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
+                    int userId = sharedPreferences.getInt("userId", -1);  // -1 est la valeur par défaut si l'ID n'est pas trouvé
+                    sentObject.put("userId", userId);
+                    sentObject.put("name", itineraryName.getText().toString());
+                    sentObject.put("itinerary", new JSONArray(selectedEnterprises));
+                    Log.i("NewRouteActivity", "JSON envoyé : " + sentObject.toString());
+
+                } catch (JSONException e) {
+                    // l'exception ne doit pas se produire
+                    toutOk = false;
+                }
+                if (toutOk) {
+                    /*
+                     * Préparation de la requête Volley. La réponse attendue est de type
+                     * JsonObject
+                     * REMARQUE : bien noter la présence du 3ème argument du constructeur qui est
+                     * l'objet Json à transmettre avec la méthode POST, en fait le body de la
+                     * requête
+                     */
+                    JsonObjectRequest requeteVolley = new JsonObjectRequest(Request.Method.POST,
+                            URL_ITINERARY, sentObject,
+                            // Ecouteur pour la réception de la réponse de la requête
+                            new com.android.volley.Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject reponse) {
+                                    // la zone de résultat est renseignée avec le résultat
+                                    // de la requête
+                                    Toast.makeText(NewRouteActivity.this, "Itinéraire crée", LENGTH_LONG).show();
+                                    // Intent intention = new Intent(NewRouteActivity.this, );
+                                }
+                            },
+                            // Ecouteur en cas d'erreur
+                            new com.android.volley.Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    error.printStackTrace();
+                                    if (error.networkResponse != null) {
+                                        // Si la réponse réseau est disponible, récupérer le code d'état et afficher les détails
+                                        Log.e("VolleyError", "Status Code: " + error.networkResponse.statusCode);
+                                        Log.e("VolleyError", "Response: " + new String(error.networkResponse.data));
+                                    } else {
+                                        // Si la réponse réseau est nulle, afficher un message d'erreur générique
+                                        Log.e("VolleyError", "Erreur réseau inconnue");
+                                    }
+                                    Toast.makeText(NewRouteActivity.this, "Erreur de connexion", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            // on ajoute un header, contenant la clé d'authentification
+                    {
+                        @Override
+                        public Map getHeaders() throws AuthFailureError {
+                            HashMap<String, String> headers = new HashMap<>();
+                            headers.put("Content-Type", "application/json");  // Ajout du Content-Type
+                            return headers;
+                        }
+                    };
+                    // ajout de la requête dans la file d'attente Volley
+                    getFileRequete().add(requeteVolley);
+                }
+            } else {
+                Toast.makeText(NewRouteActivity.this, "Erreur: pas de nom d'itinéraire", LENGTH_LONG).show();
+                Log.i("NewRouteActivity", "Erreur: pas de nom d'itinéraire");
+                itineraryLabel.setTextColor(Color.parseColor("#FF0000"));
+            }
+        } else {
+            if (itineraryName.getText().toString().isEmpty()) {
+                itineraryLabel.setTextColor(Color.parseColor("#FF0000"));
+                Log.e("NewRouteActivity", "Erreur: pas de nom d'itinéraire");
+            } else {
+                itineraryLabel.setTextColor(Color.parseColor("#FFFFFF"));
+            }
+            Toast.makeText(NewRouteActivity.this, "Erreur: aucune entreprise sélectionnée", LENGTH_LONG).show();
+            Log.e("NewRouteActivity", "Erreur: aucune entreprise sélectionnée");
+            textChoice.setTextColor(Color.parseColor("#FF0000"));
         }
     }
 
@@ -228,24 +334,30 @@ public class NewRouteActivity extends AppCompatActivity implements CheckboxSelec
         return fileRequete;
     }
 
-    // Met à jour la TextView avec les Checkbox sélectionnées
-    @Override
+    /**
+     * Met à jour l'affichage des entreprises sélectionnées
+     *
+     */
     public void onCheckboxSelectionChanged(Set<Integer> selectedItems) {
         Log.d("MainActivity", "onCheckboxSelectionChanged exécuté avec : " + selectedItems);
-        StringBuilder textSelectedEnterprises = new StringBuilder();
+
         selectedEnterprises = new ArrayList<>();
-        if (selectedItems.size() != 0) {
+        StringJoiner joiner = new StringJoiner(", ");
+
+        if (!selectedItems.isEmpty()) {
+            textChoice.setTextColor(Color.parseColor("#FFFFFF"));
             for (Integer key : selectedItems) {
-                textSelectedEnterprises.append(enterpriseValues.get(key)).append(", ");
+                joiner.add(enterpriseValues.get(key));
                 selectedEnterprises.add(key);
             }
-            displayedEnterprises.setText(textSelectedEnterprises);
+            displayedEnterprises.setText(joiner.toString());
         } else {
             displayedEnterprises.setText("");
         }
-        Log.d("MainActivity", "contenu de selectedEnterprises : " + selectedEnterprises);
 
+        Log.d("MainActivity", "contenu de selectedEnterprises : " + selectedEnterprises);
     }
+
 
     /**
      * Vérifie si l'appareil a une connexion Internet disponible
